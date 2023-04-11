14240d4bc4ae3de2a9f59aec4525e2a46ed3c197
megtb <megbickerstaff@gmail.com>
2023-04-11 17:58:04 -0400

    Adds noreferrer to map link

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             <tr>4</tr>
           </tbody>
         </table>
-      <a target="_blank" href="https://www.google.fi/maps/place/200+Park+St,+Morristown,+VT+05661/@44.5630162,-72.5946736,17z/data=!3m1!4b1!4m6!3m5!1s0x4cb5eab30507a617:0x8d898d0e54d32085!8m2!3d44.5630124!4d-72.5924849!16s%2Fg%2F11c227kw5m"><img alt="map" src="/200ParkMap.png" width="40%"/></a>
+      <a target="_blank" href="https://www.google.fi/maps/place/200+Park+St,+Morristown,+VT+05661/@44.5630162,-72.5946736,17z/data=!3m1!4b1!4m6!3m5!1s0x4cb5eab30507a617:0x8d898d0e54d32085!8m2!3d44.5630124!4d-72.5924849!16s%2Fg%2F11c227kw5m"><img alt="map" src="/200ParkMap.png" rel="noreferrer" width="40%"/></a>
       </div>
     </div>
   );
