ef15314bca4763268e9e67c5b0e06d1d91eee05e
megtb <megbickerstaff@gmail.com>
2023-04-11 17:57:06 -0400

    Adds placeholders and map/map link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='header'>
+          200 Park Street
+        </div>
       </header>
+      <div>
+        Now I need a body of some kind
+        <table>
+          <thead>here's a table</thead>
+          <tbody>
+            <tr>1</tr>
+            <tr>2</tr>
+            <tr>3</tr>
+            <tr>4</tr>
+          </tbody>
+        </table>
+      <a target="_blank" href="https://www.google.fi/maps/place/200+Park+St,+Morristown,+VT+05661/@44.5630162,-72.5946736,17z/data=!3m1!4b1!4m6!3m5!1s0x4cb5eab30507a617:0x8d898d0e54d32085!8m2!3d44.5630124!4d-72.5924849!16s%2Fg%2F11c227kw5m"><img alt="map" src="/200ParkMap.png" width="40%"/></a>
+      </div>
     </div>
   );
 }
